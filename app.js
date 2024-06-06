73d49bf1beaef801361d07c5d6116c7b1f5cdcbd
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-06-06 19:21:41 +0100

    I have added the backend and all the libaries that we will need for node

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const morgan = require('morgan')
+const bcrypt = require('bcrypt')
+const path = require("path")
+const fileURLPath = require('url')
+const __filename = fileURLPath(import.meta.url)
+const __dirname = path.__dirname(__filename)
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.set("view engine", "ejs")
+app.use(express.static("css"))
+app.use("/javascript", express.static(path.join(__dirname, "javascript")))
+app.use("/images", express.static(path.join(__dirname, "images")))
+app.use(express.urlencoded({extended:true}))
+app.use(morgan("dev"))
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log("We are listening at Port: ", port )
+})
+
+
+
