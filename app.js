9d070936d65da24504b7132915199ee0d6f5d029
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-06-08 20:55:59 +0100

    Connected all the folders together in the backend

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const port = process.env.PORT || 3000
 
 app.set("view engine", "ejs")
 app.use(express.static("css"))
-app.use("/javascript", express.static(path.join(__dirname, "javascript")))
-app.use("/images", express.static(path.join(__dirname, "images")))
+app.use(express.static("JavaScript"))
+app.use(express.static("Images"))
 app.use(express.urlencoded({extended:true}))
 app.use(morgan("dev"))
 app.use(express.json())
